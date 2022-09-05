bf02717365f3365a25235e5984b2cbcd87503505
Bruno Luís Bahiense <brunoluisbahiense@gmail.com>
2022-09-05 15:56:58 -0300

    test

--- src/connect.js
+++ src/connect.js
@@ -1,7 +1,10 @@
 const axios = require('axios')
 
 const instanceAxios = axios.create({
-    baseURL: 'https://integrador.saltzap.com/webhook/85929578-91bb-4003-b3d9-41c586eb6cbe'
+    baseURL: 'https://integrador.saltzap.com/webhook/85929578-91bb-4003-b3d9-41c586eb6cbe',
+    params:{
+        api_key: ''
+    }
 })
 
 module.exports = instanceAxios
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const app = express()
 const cors = require('cors')
 
-const routes = require('./routes');
+const routes = require('./routes')
 app.use(cors())
 
-app.use(express.json());
-app.use(routes);
+app.use(express.json())
+app.use(routes)
 app.listen(process.env.PORT || 8000)
